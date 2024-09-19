2e12ce1861cf1e2810a7f81f4740f5be29f8a3e5
Zrrex12 <swir3412@gmail.com>
2024-09-19 15:00:54 -0600

    Add files via upload

---
+++ Unidad I/Modulo2/11450. Evaluando una fórmula gigante.py
@@ -0,0 +1,15 @@
+'''datos = input("")
+x,y,z,a,b=datos.split()
+x=float(x)
+y=float(y)
+z=float(z)
+a=float(a)
+b=float(b)
+c=x+y+z+a+b
+print(c/5)'''
+dato = input("")
+x = int(dato)
+if x%2==0:
+    print("Es par")
+else:
+    print("Es impar")
