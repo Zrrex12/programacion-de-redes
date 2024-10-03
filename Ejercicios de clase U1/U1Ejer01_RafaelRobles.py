9fcc5d18602b5dd86f06e80675a7bb789fee18b9
Zrrex12 <swir3412@gmail.com>
2024-10-03 14:55:13 -0600

    Add files via upload

---
+++ Ejercicios de clase U1/U1Ejer01_RafaelRobles.py
@@ -0,0 +1,31 @@
+''' Nombre: Rafael Robles Gonzalez
+    Descripcion: Fundamentos Python
+    Fecha: 03/10/24'''
+
+# Escribe la instrucción para desplegar el título
+print("Ejercicio 1 – Fundamentos de Python")
+#Escribe la instrucción para desplegar el título “Autor – [Nombre del participante]”
+print("Autor - Rafael Robles Gonzalez")
+# Escribe la instrucción para realizar un salto de línea
+print()
+#Declara una variable de tipo real y asigna el valor 4
+variable1 = 8
+#Declara una variable de tipo real y asigna el valor 3
+variable2 = 2
+# Imprime el contenido de las dos variables
+print("El valor de la variable1 es:", variable1)
+print("El valor de la variable2 es:", variable2)
+# Escribe la instrucción para realizar un salto de línea
+print()
+#Haz el cálculo de la suma y asígnala a una variable llamada suma; haz lo mismo para
+#las operaciones de resta, multiplicación, división y potenciación
+suma = variable1 + variable2
+resta = variable1 - variable2
+multiplicacion = variable1 * variable2
+division = variable1 / variable2
+potencia = variable1 ** variable2
+print("Suma: ", suma)
+print("Resta: ", resta)
+print("Multiplicacion: ", multiplicacion)
+print("Division: ", division)
+print("Potencia: ", potencia)
